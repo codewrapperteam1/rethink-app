241f452c1625fcaaa86ed5c736094506727ae64a
yhvhgamer <fiverrdirectboss@hotmail.com>
2026-04-16 02:09:50 -0400

    Add USQUE WARP preferences to PersistentState.kt

---
+++ PersistentState.kt
@@ -0,0 +1,25 @@
+    const val PREF_USQUE_ENABLED = "pref_usque_enabled"
+    const val PREF_USQUE_REGISTERED = "pref_usque_registered"
+    const val PREF_USQUE_TUN_IF = "pref_usque_tun_if"
+    const val PREF_USQUE_TUN_IP4 = "pref_usque_tun_ipv4"
+    const val PREF_USQUE_TUN_IP6 = "pref_usque_tun_ipv6"
+
+    var usqueEnabled: Boolean
+        get() = sharedPreferences.getBoolean(PREF_USQUE_ENABLED, false)
+        set(value) = sharedPreferences.edit().putBoolean(PREF_USQUE_ENABLED, value).apply()
+
+    var usqueRegistered: Boolean
+        get() = sharedPreferences.getBoolean(PREF_USQUE_REGISTERED, false)
+        set(value) = sharedPreferences.edit().putBoolean(PREF_USQUE_REGISTERED, value).apply()
+
+    var usqueTunInterface: String?
+        get() = sharedPreferences.getString(PREF_USQUE_TUN_IF, null)
+        set(value) = sharedPreferences.edit().putString(PREF_USQUE_TUN_IF, value).apply()
+
+    var usqueTunIpv4: String?
+        get() = sharedPreferences.getString(PREF_USQUE_TUN_IP4, null)
+        set(value) = sharedPreferences.edit().putString(PREF_USQUE_TUN_IP4, value).apply()
+
+    var usqueTunIpv6: String?
+        get() = sharedPreferences.getString(PREF_USQUE_TUN_IP6, null)
+        set(value) = sharedPreferences.edit().putString(PREF_USQUE_TUN_IP6, value).apply()
